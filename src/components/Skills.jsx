fc911aa6b80408042d275b73d3c30985bdf8d957
mattsayco <mattsayco@gmail.com>
2024-01-11 17:36:25 +0800

    Finished About me and Tech Stack

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,3 +1,24 @@
-export default function Skills() {
-  return <></>;
+/* eslint-disable react/prop-types */
+import { IconContext } from "react-icons";
+export default function Skills({ headerIcon, title, skillsArr }) {
+  return (
+    <IconContext.Provider value={{ className: "icon-provider" }}>
+      <div className="skills-main">
+        <div className="header-container">
+          <span className="header-icon">{headerIcon}</span>
+        </div>
+        <h1>{title}</h1>
+        <div className="skill-container">
+          {skillsArr.map(({ title, icon }) => {
+            return (
+              <div className="skill-icon-container" key={title}>
+                <span>{icon}</span>
+                <p>{title}</p>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </IconContext.Provider>
+  );
 }
